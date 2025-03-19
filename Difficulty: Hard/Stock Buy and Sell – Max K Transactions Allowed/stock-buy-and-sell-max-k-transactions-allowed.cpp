e15a1bcc79787dd70f2287e63bdafca5caaa8cc9
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2025-03-19 22:35:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices, int k) {
+        // code here
+        int n = prices.size();
+        if (n == 0 || k == 0) return 0;
+        
+        // If k is very large, reduce to infinite transactions case
+        if (2 * k > n) {
+            int maxProfit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+
+        // DP table
+        vector<vector<int>> dp(k + 1, vector<int>(n, 0));
+
+        for (int i = 1; i <= k; i++) {
+            int prevMax = -prices[0];  // Max profit if we buy on some previous day
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = max(dp[i][j - 1], prices[j] + prevMax);
+                prevMax = max(prevMax, dp[i - 1][j] - prices[j]);
+            }
+        }
+
+        return dp[k][n - 1];
+    }
+};
+
+        
+
+
+//{ Driver Code Starts.
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        istringstream iss(input);
+        vector<int> arr;
+        int num;
+
+        // Read integers from the input string
+        while (iss >> num) {
+            arr.push_back(num);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        Solution ob;
+        cout << ob.maxProfit(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
