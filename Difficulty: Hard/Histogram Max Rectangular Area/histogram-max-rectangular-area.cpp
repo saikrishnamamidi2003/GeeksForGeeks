439baf7dad8947d350053afc69a2db4cc4cb99a6
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2025-03-23 12:13:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int getMaxArea(vector<int> &arr) {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> left(n), right(n);
+
+        // Finding Nearest Smaller to Left (NSL)
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            left[i] = (st.empty()) ? -1 : st.top();
+            st.push(i);
+        }
+
+        // Clear stack for next computation
+        while (!st.empty()) st.pop();
+
+        // Finding Nearest Smaller to Right (NSR)
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            right[i] = (st.empty()) ? n : st.top();
+            st.push(i);
+        }
+
+        // Calculate max area
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            maxArea = max(maxArea, arr[i] * width);
+        }
+
+        return maxArea;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.getMaxArea(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
